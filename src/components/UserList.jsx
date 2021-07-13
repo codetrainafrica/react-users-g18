a7c03549cf5244f0d0c71fac937a33dd91b2b20b
zak <codedpills@gmail.com>
2021-07-13 13:06:28 +0000

    pass props for edit and delete to User component

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import User from "./User";
 
-const UserList = ({ users, deleteUser }) => {
+const UserList = ({ users, deleteUser, editUser }) => {
   const userList = users.map((user) => {
-    return <User user={user} key={user.id} deleteUser={deleteUser} />;
+    return (
+      <User
+        user={user}
+        key={user.id}
+        deleteUser={deleteUser}
+        editUser={editUser}
+      />
+    );
   });
   return <div className="user-list">{userList}</div>;
 };
