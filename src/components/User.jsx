d2f6b72f1c4ca9ef6f6498be3ca39c73a3716b33
zak <codedpills@gmail.com>
2021-07-13 11:51:45 +0000

    add delete functionality

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup'
+import React from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
 
-const User = ({ user }) => {
-    return (
-        <div className="user">
-            <Card>
-                <Card.Header>{user.name}</Card.Header>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>Email: {user.email}</ListGroup.Item>
-                    <ListGroup.Item>Gen: {user.gen}</ListGroup.Item>
-                </ListGroup>
-            </Card>
-        </div>
-    );
-}
+const User = ({ user, deleteUser }) => {
+  const handleDelete = (e) => {
+    deleteUser(user.id);
+  };
+  return (
+    <div className="user">
+      <Card className="mb-4">
+        <Card.Header>{user.name}</Card.Header>
+        <ListGroup variant="flush">
+          <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+          <ListGroup.Item>Gen: {user.gen}</ListGroup.Item>
+          <button className="btn btn-primary">Edit</button>
+          <button className="btn btn-default" onClick={handleDelete}>
+            Delete
+          </button>
+        </ListGroup>
+      </Card>
+    </div>
+  );
+};
 
 export default User;
