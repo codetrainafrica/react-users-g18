aa7731f77a12599e8dc63ec2983413d8fa21d874
zak <codedpills@gmail.com>
2021-08-24 12:23:28 +0000

    add update/delete functionality

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,4 +1,4 @@
-import { ADD_USER } from "../actions/userActions";
+import { ADD_USER, DELETE_USER, EDIT_USER } from "../actions/userActions";
 
 const initialState = {
   users: [
@@ -16,7 +16,15 @@ const usersReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_USER:
       return { ...state.users, users: [...state.users, action.payload] };
-
+    case DELETE_USER:
+      const savedUsers = state.users.filter((user) => {
+        return user.id !== action.payload;
+      });
+      return {...state, users: savedUsers};
+    case EDIT_USER:
+      return {...state, users: state.users.map((user) =>
+        user.id === action.payload.id ? action.payload : user
+      )}
     default:
       return state;
   }
